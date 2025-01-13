0a5f8b9cf924b9bb085d24f4c75cbdba38917568
di-birt <mardanza@birt.eus>
2025-01-13 09:51:10 +0100

    crear rama

---
+++ src/app/pages/tab2/tab2.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Article } from 'src/app/interfaces/interfaces';
+import { GestionNoticiasLeerService } from 'src/app/services/gestion-noticias-leer.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  standalone: false
+})
+export class Tab2Page {
+
+  // Creo e inicializo un array vacío
+  listaNoticias: Article[] = [];
+
+  constructor(public gestionNoticiasLeer: GestionNoticiasLeerService) {}
+
+}
